7b6721f5461a042b0d92295663a8c5adba0bf735
ErickdelaCruzCas <erickdlcruzcas@gmail.com>
2025-12-24 18:09:12 +0100

    feat: recover app

--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -1,17 +1,17 @@
-package main
+// package main
 
-import (
-	"github.com/codecrafters-io/shell-starter-go/internal/command"
-	"github.com/codecrafters-io/shell-starter-go/internal/shell"
-)
+// import (
+// 	"github.com/codecrafters-io/shell-starter-go/internal/command"
+// 	"github.com/codecrafters-io/shell-starter-go/internal/shell"
+// )
 
-func main() {
-	commands := map[string]command.Command{
-		"exit": command.ExitCommand{},
-		"echo": command.EchoCommand{},
-	}
+// func main() {
+// 	commands := map[string]command.Command{
+// 		"exit": command.ExitCommand{},
+// 		"echo": command.EchoCommand{},
+// 	}
 
-	sh := shell.New(commands)
-	commands["type"] = command.NewTypeCommand(sh.IsBuiltin)
-	sh.Run()
-}
+// 	sh := shell.New(commands)
+// 	commands["type"] = command.NewTypeCommand(sh.IsBuiltin)
+// 	sh.Run()
+// }
