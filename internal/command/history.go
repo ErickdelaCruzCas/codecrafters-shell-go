0e3579b9839c7bd4eca7f67d1aafe0aed1551e82
ErickdelaCruzCas <erickdlcruzcas@gmail.com>
2026-01-02 16:48:19 +0100

    limit

--- internal/command/history.go
+++ internal/command/history.go
@@ -3,6 +3,7 @@ package command
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/codecrafters-io/shell-starter-go/internal/history"
 )
@@ -23,8 +24,19 @@ func (h HistoryCommand) Name() string {
 
 func (h HistoryCommand) Execute(ctx context.Context, args []string, io IO) Result {
 	entries := h.store.List()
-	for i, entry := range entries {
-		fmt.Fprintf(io.Stdout, "%5d  %s\n", i+1, entry)
+	start := 0
+	if len(args) > 0 {
+		limit, err := strconv.Atoi(args[0])
+		if err != nil || limit < 0 {
+			return Ok
+		}
+		if limit < len(entries) {
+			start = len(entries) - limit
+		}
+	}
+
+	for i, entry := range entries[start:] {
+		fmt.Fprintf(io.Stdout, "%5d  %s\n", start+i+1, entry)
 	}
 
 	return Ok
